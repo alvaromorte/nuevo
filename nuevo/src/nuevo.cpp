cdd6afd39c2aab7a96f316d09f9a870212d2e257
alvaromorte <alvaromorte@gmail.com>
2016-10-22 17:17:18 +0200

    incorporoo...
    zxfsdfsdfsdfsfsdasdfghjklñ

---
+++ nuevo/src/nuevo.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : nuevo.cpp
+// Author      : AMM
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!! ...........nuevoHello World !!!" << endl; // prints !!! Hello World !!!
+	return 0;
+}
